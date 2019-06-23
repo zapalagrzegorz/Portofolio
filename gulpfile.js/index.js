4e4fb04135854a44eea38727281e25ca23811d0f
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-06-23 19:11:11 +0200

    saving progress

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -6,13 +6,13 @@
  */
 
 const settings = {
-    clean: true,
-    scripts: true,
-    polyfills: true,
-    styles: true,
-    svgs: true,
-    copy: true,
-    reload: true
+  clean: true,
+  scripts: true,
+  polyfills: true,
+  styles: true,
+  svgs: true,
+  copy: true,
+  reload: true
 };
 
 exports.settings = settings;
@@ -22,26 +22,25 @@ exports.settings = settings;
  */
 
 const paths = {
-    input: "src/",
-    output: "dist/",
-    scripts: {
-        input: "src/**/*.js",
-        polyfills: ".polyfill.js",
-        output: "dist/js/"
-    },
-    styles: {
-        input: "src/sass/**/*.{scss,sass}",
-        output: "dist/css/"
-    },
-    svgs: {
-        input: "src/svg-sprite/*.svg",
-        output: "dist/svg/"
-    },
-    copy: {
-        input: "src/copy/**/*",
-        output: "dist/"
-    },
-    reload: "./dist/"
+  input: "src/",
+  output: "dist/",
+  scripts: {
+    input: "src/**/*.js",
+    output: "dist/js/"
+  },
+  styles: {
+    input: "src/sass/**/*.{scss,sass}",
+    output: "dist/css/"
+  },
+  svgs: {
+    input: "src/svg-sprite/*.svg",
+    output: "dist/svg/"
+  },
+  copy: {
+    input: "src/copy/**/*",
+    output: "dist/"
+  },
+  reload: "./dist/"
 };
 exports.paths = paths;
 
@@ -57,8 +56,6 @@ const rename = require("gulp-rename");
 
 // Scripts
 const babel = require("gulp-babel");
-// const jshint = require("gulp-jshint");npm install gulp-eslint
-// const stylish = require("jshint-stylish");
 const eslint = require("gulp-eslint");
 const concat = require("gulp-concat");
 const uglify = require("gulp-terser");
@@ -71,7 +68,7 @@ const prefix = require("gulp-autoprefixer");
 const minify = require("gulp-cssnano");
 
 // SVGs
-const svgmin = require("gulp-svgmin");
+// const svgmin = require("gulp-svgmin");
 const svgo = require("gulp-svgo");
 const svgstore = require("gulp-svgstore");
 
@@ -81,146 +78,137 @@ const browserSync = require("browser-sync");
 const fileinclude = require("gulp-file-include");
 const { cleanDist } = require("./cleanDist");
 
-// Repeated JavaScript tasks
-
 // Lint, minify, and concatenate scripts
 function buildScripts(done) {
-    // Make sure this feature is activated before running
-    if (!settings.scripts) return done();
+  // Make sure this feature is activated before running
+  if (!settings.scripts) return done();
 
-    // Run tasks on script files
-    return src(paths.scripts.input)
-        .pipe(
-            gulpif(argv.production, babel({ presets: ["@babel/preset-env"] }))
-        )
-        .pipe(gulpif(argv.production, uglify()))
-        .pipe(concat("main.js"))
-        .pipe(dest(paths.scripts.output));
+  // Run tasks on script files
+  return src(paths.scripts.input)
+    .pipe(gulpif(argv.production, babel({ presets: ["@babel/preset-env"] })))
+    .pipe(gulpif(argv.production, uglify()))
+    .pipe(concat("main.js"))
+    .pipe(dest(paths.scripts.output));
 }
 
 // Lint scripts
 function lintScripts(done) {
-    // Make sure this feature is activated before running
-    if (!settings.scripts) return done();
+  // Make sure this feature is activated before running
+  if (!settings.scripts) return done();
 
-    // Lint scripts
-    return src(paths.scripts.input)
-        .pipe(
-            eslint({
-                configFile: "./.eslintrc.json"
-            })
-        )
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+  // Lint scripts
+  return src(paths.scripts.input)
+    .pipe(
+      eslint({
+        configFile: "./.eslintrc.json"
+      })
+    )
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 }
 
 // Process, lint, and minify Sass files
 function buildStyles(done) {
-    // Make sure this feature is activated before running
-    if (!settings.styles) return done();
+  // Make sure this feature is activated before running
+  if (!settings.styles) return done();
 
-    // Run tasks on all Sass files
-    return src(paths.styles.input)
-        .pipe(
-            sass({
-                outputStyle: "expanded",
-                sourceComments: true,
-                includePaths: "node_modules/bootstrap/scss"
-            })
-        )
-        .pipe(
-            prefix({
-                cascade: true,
-                remove: true
-            })
-        )
-        .pipe(dest(paths.styles.output))
-        .pipe(rename({ suffix: ".min" }))
-        .pipe(
-            minify({
-                discardComments: {
-                    removeAll: true
-                }
-            })
-        )
-        .pipe(dest(paths.styles.output));
+  // Run tasks on all Sass files
+  return src(paths.styles.input)
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+        sourceComments: true,
+        includePaths: "node_modules/bootstrap/scss"
+      })
+    )
+    .pipe(
+      prefix({
+        cascade: true,
+        remove: true
+      })
+    )
+    .pipe(dest(paths.styles.output))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(
+      minify({
+        discardComments: {
+          removeAll: true
+        }
+      })
+    )
+    .pipe(dest(paths.styles.output));
 }
 
-// Optimize SVG files
-function buildSVGs(done) {
-    // Make sure this feature is activated before running
-    if (!settings.svgs) return done();
+function svgSprite() {
+  return src(paths.svgs.input)
+    .pipe(
+      svgo({
+        plugins: [
+          { removeDoctype: true },
+          { removeXMLProcInst: true },
+          { removeComments: true },
+          { removeMetadata: true },
+          { removeDesc: true },
+          { removeUselessDefs: true },
+          { removeTitle: true },
+          { convertPathData: { floatPrecision: 2 } },
+          { removeViewBox: false }
+        ]
+      })
+    )
+    .pipe(rename({ prefix: "image-" }))
+    .pipe(svgstore())
+    .pipe(dest(paths.svgs.output));
+}
 
-    // Optimize SVG files
-    return src(paths.svgs.input)
-        .pipe(svgmin())
-        .pipe(dest(paths.svgs.output));
+// Robi inline czego trzeba w src-master.hbs i zapisuje jako master.hbs
+function inlineComponents() {
+  return src("dist/index.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file"
+      })
+    )
+    .pipe(dest("./dist"));
 }
 
 // Copy static files into output folder
 function copyFiles(done) {
-    // Make sure this feature is activated before running
-    if (!settings.copy) return done();
+  // Make sure this feature is activated before running
+  if (!settings.copy) return done();
 
-    // Copy static files
-    return src(paths.copy.input).pipe(dest(paths.copy.output));
+  // Copy static files
+  return src(paths.copy.input).pipe(dest(paths.copy.output));
 }
 
 // Watch for changes to the src directory
 function startServer(done) {
-    // Make sure this feature is activated before running
-    if (!settings.reload) return done();
+  // Make sure this feature is activated before running
+  if (!settings.reload) return done();
 
-    // Initialize BrowserSync
-    browserSync.init({
-        server: {
-            baseDir: paths.reload
-        }
-    });
+  // Initialize BrowserSync
+  browserSync.init({
+    server: {
+      baseDir: paths.reload
+    }
+  });
 
-    // Signal completion
-    return done();
+  // Signal completion
+  return done();
 }
 
 // Reload the browser when files change
 function reloadBrowser(done) {
-    if (!settings.reload) return done();
-    browserSync.reload();
-    return done();
+  if (!settings.reload) return done();
+  browserSync.reload();
+  return done();
 }
 
 // Watch for changes
 function watchSource(done) {
-    watch(paths.input, series(exports.default, reloadBrowser));
-    return done();
-}
-
-function svgSprite() {
-    return src(paths.svgs.input)
-        .pipe(
-            svgo({
-                plugins: [
-                    { removeTitle: true },
-                    { convertPathData: { floatPrecision: 2 } },
-                    { removeViewBox: false }
-                ]
-            })
-        )
-        .pipe(rename({ prefix: "image-" }))
-        .pipe(svgstore({ inlineSvg: true }))
-        .pipe(dest(paths.svgs.output));
-}
-
-// Robi inline czego trzeba w src-master.hbs i zapisuje jako master.hbs
-function inlineSVG() {
-    return src("dist/index.html")
-        .pipe(
-            fileinclude({
-                prefix: "@@",
-                basepath: "@file"
-            })
-        )
-        .pipe(dest("./dist"));
+  watch(paths.input, series(exports.default, reloadBrowser));
+  return done();
 }
 
 /**
@@ -230,17 +218,18 @@ function inlineSVG() {
 // Default task
 // gulp
 exports.default = series(
-    cleanDist,
-    parallel(lintScripts, buildScripts, buildStyles, svgSprite, copyFiles),
-    inlineSVG
+  cleanDist,
+  parallel(lintScripts, buildScripts, buildStyles, svgSprite, copyFiles),
+  inlineComponents
 );
 
 exports.build = series(
-    cleanDist,
-    parallel(lintScripts, buildScripts, buildStyles, buildSVGs, copyFiles)
+  cleanDist,
+  parallel(lintScripts, buildScripts, buildStyles, svgSprite, copyFiles),
+  inlineComponents
 );
 
-exports.svgSprite = series(svgSprite);
+exports.svgSprite = svgSprite;
 
 // Watch and reload
 // gulp watch
